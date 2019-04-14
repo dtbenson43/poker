6600da3892aa9a5f6cf7706b8d8523c2628b6bba
DubbAR7 <DubbAR7@users.noreply.github.com>
2019-04-14 11:52:36 -0500

    Updated Betting Rounds

--- src/main/java/com/poker/PokerController.java
+++ src/main/java/com/poker/PokerController.java
@@ -1,6 +1,7 @@
 package com.poker;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import org.drools.KnowledgeBase;
 import org.drools.KnowledgeBaseFactory;
@@ -58,10 +59,17 @@ public class PokerController {
     	private ArrayList<Card> PlayerHand;
     	private ArrayList<Card> Board;
     	private ArrayList<Card> Deck;
+    	private int playerChips = 1000;
+    	private int agentChips = 1000;
+    	private int smallBlind = 25;
+    	private int bigBlind = 50;
+    	private int pot = 0;
+    	private Scanner scnr = new Scanner(System.in);
     	
     	private int turn;
     	
     	public Game() {
+    		
     		this.turn = 0;
     		if(Math.random() < 0.5) {
     			this.turn = 1;
@@ -71,34 +79,76 @@ public class PokerController {
     	}
     	
     	private void newHand() {
-        	ArrayList<Card> AgentHand = new ArrayList<Card>();
-        	ArrayList<Card> PlayerHand = new ArrayList<Card>();
-        	ArrayList<Card> Board = new ArrayList<Card>();
+        	AgentHand = new ArrayList<Card>();
+        	PlayerHand = new ArrayList<Card>();
+        	Board = new ArrayList<Card>();
         	
     		this.buildDeck();
     		this.dealCards();
     	}
     	
     	private void buildDeck() {
-    		ArrayList<Card> Deck = new ArrayList<Card>();
+    		Deck = new ArrayList<Card>();
     		
-    		for(int i = 0; i < 3; i++) {
-    			for(int j = 0; j < 12; j++) {
+    		for(int i = 0; i < 4; i++) {
+    			for(int j = 0; j < 13; j++) {
     				Deck.add(new Card(i, j));
     			}
     		}
+    		System.out.println(Deck.size());
     	}
     	
     	private void dealCards() {
+ 
     		PlayerHand.add(Deck.remove((int)(Math.random() * 1000000) % Deck.size()));
     		PlayerHand.add(Deck.remove((int)(Math.random() * 1000000) % Deck.size()));
     		
     		AgentHand.add(Deck.remove((int)(Math.random() * 1000000) % Deck.size()));
     		AgentHand.add(Deck.remove((int)(Math.random() * 1000000) % Deck.size()));
     		
-    		Board.add(Deck.remove((int)(Math.random() * 1000000) % Deck.size()));
-    		Board.add(Deck.remove((int)(Math.random() * 1000000) % Deck.size()));
-    		Board.add(Deck.remove((int)(Math.random() * 1000000) % Deck.size()));
+    	}
+    	
+    	private void bettingLoop() {
+    		
+    		playerBet();
+    		botBet();
+    	}
+    	
+    	private void playerBet(int chipsToCall) {
+    		System.out.println("Your Cards:");
+    		System.out.println("Enter your choice (case sensitive):");
+    		if(chipsToCall > 0) {
+	    		System.out.println("1. Call " + chipsToCall + " chips");
+	    		System.out.println("2. Raise");
+	    		System.out.println("3. Fold");
+	    		bettingRules(scnr.nextInt(), chipsToCall);
+    		}
+    	}
+    	
+    	private void bettingRules(int choice, int chipsToCall) {
+    		int bet = 0;
+    		
+    		if(choice == 1) {
+    			pot += chipsToCall;
+    			playerChips -= chipsToCall;
+    		}
+    		if(choice == 2) {
+    			System.out.println("Enter the amount you want to raise: ");
+    			bet = scnr.nextInt();
+    			while(bet < bigBlind) {
+    				System.out.println("Raise must be at least" + bigBlind + " chips");
+    				bet = scnr.nextInt();
+    			}
+    			if((chipsToCall + bet) > playerChips) {
+    				bet = playerChips - chipsToCall;
+    			}
+    			pot += chipsToCall + bet;
+    			playerChips -= chipsToCall + bet;
+    		}
+    		if(choice == 3) {
+    			agentChips += pot;
+    			pot = 0;
+    		}
     	}
 
 		public int getTurn() {
